5d6d05bf4efc76f0537bdcde124bd7d1af40a419
anfigeno <dolorcriticodevastador@proton.me>
2026-06-07 01:23:53 -0500

    Plugin para yazi

--- modulos/entornoDeDesarrollo/yazi/default.nix
+++ modulos/entornoDeDesarrollo/yazi/default.nix
@@ -2,6 +2,7 @@
   config,
   usuario,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -10,6 +11,23 @@
   config = lib.mkIf config.anfistema.entornoDeDesarrollo.yazi.activar {
     home-manager.users.${usuario}.programs.yazi = {
       enable = true;
+      extraPackages = with pkgs; [
+        wl-clipboard
+      ];
+      plugins = with pkgs.yaziPlugins; {
+        wl-clipboard = wl-clipboard;
+      };
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            on = "y";
+            run = [
+              "plugin wl-clipboard"
+              "yank"
+            ];
+          }
+        ];
+      };
     };
   };
 }
