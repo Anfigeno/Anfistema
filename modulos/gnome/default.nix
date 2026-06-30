e80b0c87f14348abb6fe1eaae27f696b3d6d410f
anfigeno <dolorcriticodevastador@proton.me>
2026-06-30 15:48:37 -0500

    Configuración de escritorio remoto

--- modulos/gnome/default.nix
+++ modulos/gnome/default.nix
@@ -131,5 +131,16 @@
           );
         };
       };
+
+    services.gnome.gnome-remote-desktop.enable = true;
+    systemd.services.gnome-remote-desktop.wantedBy = [ "graphical.target" ];
+    networking.firewall.allowedTCPPorts = [ 3389 ];
+    services.displayManager.autoLogin.enable = false;
+    services.getty.autologinUser = null;
+
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
   };
 }
